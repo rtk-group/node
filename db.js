55b81c617bd9b12938976d5b043fbd00aee3a58e
rtk-group <mydevclass9690@gmail.com>
2024-09-14 05:42:51 -0700

    first update

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-// const mongourl = 'mongodb://127.0.0.1:27017/hotels';
+// const mongourl = process.env.local_url;
 const mongourl = process.env.mongodb_url;
 // set up to mongodb connection
 mongoose.connect(mongourl,{
